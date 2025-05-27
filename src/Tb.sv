b9b143eff59b133dffac20b91c79bc1e554b6a71
Lucas G <lucasgomes062005@gmail.com>
2025-05-27 14:51:02 -0300

    mudei o tb

--- src/Tb.sv
+++ src/Tb.sv
@@ -26,15 +26,36 @@ TOP dut{
 always #500 clk = ~clk; // 1MHz = 1us = 1000ns, muda o sinal na metade do periodo total (500ns).
 
 initial begin
-    always@(posedge clk) begin
-        if(status_out) begin
-            write_in = 1;
-            data_in = data_in + 1;
-        end else begin
-            write_in = 0;
-        end
-    end
-end
+        clk = 0;
+        rst = 1;
+        data_in = 0;
+        write_in = 0;
+        enqueue_in = 0;
+        dequeue_in = 0;
 
+        #2000;
+        rst = 0;
+
+        #5000;
+
+        for (int i = 0; i < 8; i++) begin // manda de 0 a 7 esperando o status_out ficar alto
+            wait (status == 1);   
+            write_in = 1;
+            data_in = i[0];       
+            #1000;                
+            write_in = 0;
+            #1000;
+        end
+        
+        #10000;
+
+        dequeue_in = 1; // manda retirar da fila
+        #1000;
+        dequeue_in = 0;
+
+        #10000;
+
+        $finish;
+    end
 
 endmodule
